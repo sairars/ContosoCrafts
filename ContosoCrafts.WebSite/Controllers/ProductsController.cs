7c4ab85c44458924dde62e1742a0d0574c66983f
Saira Rashid <saira.rashid1216@gmail.com>
2023-03-23 09:13:48 -0400

    Initial commit of project

---
+++ ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private JsonFileProductService _productService;
+        public ProductsController(JsonFileProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {   
+            return _productService.GetProducts(); 
+        }
+
+        [Route("Ratings")]
+        [HttpGet]
+        public IActionResult GetRatings([FromQuery] string productId, [FromQuery] int rating)
+        {
+            _productService.AddRating(productId, rating);
+            return Ok();
+        }       
+    }
+}
---
+++ ContosoCrafts.WebSite/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private JsonFileProductService _productService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = _productService.GetProducts();
+        }
+    }
+}
